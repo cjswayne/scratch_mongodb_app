04e784fad0afc7c4868e672ca8e521d184d5e2f7
cjswayne <134882908+cjswayne@users.noreply.github.com>
2024-02-13 17:39:00 -0800

    finished class

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,6 @@
 /** Setup the Mongoose connection **/
 const mongoose = require('mongoose');
-// require('dotenv').config(); 
 const DB_URL = process.env.DB_URL;
-console.log('Connecting to DB with URL:', process.env.DB_URL);
-
-
-
 
 mongoose.connect(DB_URL);
 
